9b22df436787344b28259e24a8cdd8057368ed9b
hibiki-is-mine <1634503446@qq.com>
2023-09-05 18:26:12 +0800

    完成SpringSecurity,准备进行activiti7开发

---
+++ service_oa/src/test/java/com/test/oa/RedisTest.java
@@ -0,0 +1,17 @@
+package com.test.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@SpringBootTest
+public class RedisTest {
+    @Autowired
+    StringRedisTemplate redisTemplate;
+    @Test
+    public void test(){
+        redisTemplate.opsForValue().set("testk1","testv1");
+
+    }
+}
